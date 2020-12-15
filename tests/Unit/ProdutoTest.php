ce2ce24225419422627b085227f339f61dd8afa5
Walquirio Saraiva Rocha <walquiriosaraiva@gmail.com>
2020-12-14 23:42:29 -0300

    teste produto

---
+++ tests/Unit/ProdutoTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use App\Produto;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testFindById()
+    {
+        $produtos = new Produto();
+        $produto = $produtos->findById(1);
+        $this->assertEquals("Nome do Produto", $produto->nome)
+    }
+}
